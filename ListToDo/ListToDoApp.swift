0283c1ca7bf7a9e1dd7b1eb0415a82a931e277a0
PiotrK <piotr.koscielny69@gmail.com>
2025-06-10 08:47:17 +0200

    Initial Commit

---
+++ ListToDo/ListToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListToDoApp.swift
+//  ListToDo
+//
+//  Created by piotr koscielny on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ListToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
