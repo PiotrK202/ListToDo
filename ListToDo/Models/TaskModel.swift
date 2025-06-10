fc72c1f009e718a85d01ccae1945ff874cb048a3
PiotrK <piotr.koscielny69@gmail.com>
2025-06-10 09:10:51 +0200

    added Model for Task

--- ListToDo/Models/TaskModel.swift
+++ ListToDo/Models/TaskModel.swift
@@ -5,4 +5,58 @@
 //  Created by piotr koscielny on 10/6/25.
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
+import UniformTypeIdentifiers
+
+@Model
+final class TaskModel: Identifiable, Hashable {
+    @Attribute(.unique) var id: UUID
+    var title: String
+    var taskDescription: String
+    var type: TaskType
+    var notificationDate: Date?
+    var createdAt: Date
+    
+    var transferable: TransferableTask {
+        TransferableTask(id: id, title: title,
+                         description: taskDescription,
+                         type: type,
+                         notificationDate: notificationDate)
+    }
+    
+    init(id: UUID, title: String, taskDescription: String, type: TaskType, notificationDate: Date? = nil, createdAt: Date = Date()) {
+        self.id = id
+        self.title = title
+        self.taskDescription = taskDescription
+        self.type = type
+        self.notificationDate = notificationDate
+        self.createdAt = createdAt
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct TransferableTask: Codable, Transferable {
+    let id: UUID
+    let title: String
+    let description: String
+    let type: TaskType
+    let notificationDate: Date?
+    
+    static var transferRepresentation: some TransferRepresentation {
+        CodableRepresentation(contentType: .task)
+    }
+}
+
+extension UTType {
+    static let task = UTType(exportedAs: "com.ListToDo.task")
+}
+
+enum TaskType: String, Codable {
+    case important
+    case regular
+    case completed
+}
