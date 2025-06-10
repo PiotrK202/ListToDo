f6867761937c4012618e69e5b7299e757c3af2cd
PiotrK <piotr.koscielny69@gmail.com>
2025-06-10 09:31:26 +0200

    added NotyficationHelper

--- ListToDo/Helpers/NotyficationHelper.swift
+++ ListToDo/Helpers/NotyficationHelper.swift
@@ -5,4 +5,44 @@
 //  Created by piotr koscielny on 10/6/25.
 //
 
-import Foundation
+import SwiftUI
+import UserNotifications
+
+@Observable
+final class NotyficationHelper {
+    var showPermissionAlert = false
+    
+    func requestPermission() async -> Bool {
+        let center = UNUserNotificationCenter.current()
+        let settings = await center.notificationSettings()
+        
+        switch settings.authorizationStatus {
+        case .notDetermined:
+            let granted = try? await center.requestAuthorization(options: [.alert, .badge, .sound])
+            if granted == true {
+                return true
+            } else {
+                showPermissionAlert = true
+                return false
+            }
+        case .denied:
+            showPermissionAlert = true
+            return false
+        case .authorized, .provisional, .ephemeral:
+            return true
+        default:
+            showPermissionAlert = true
+            return false
+        }
+    }
+    
+    func openAppSettings() {
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    func extiApp() {
+        exit(0)
+    }
+}
